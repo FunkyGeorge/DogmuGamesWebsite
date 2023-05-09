f6d1c09d7fa2e9bed0c5376451cc2525393112d7
George Miranda <george92miranda@gmail.com>
2023-05-08 23:16:44 -0700

    add Honed By Stone

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from "./components/header/Header";
 import WootBlock from './components/wootblock/WootBlock';
-import EmailSignup from './components/emailsignup/EmailSignup';
+import HonedByStone from './components/HonedByStone/HonedByStone';
 import WootSteam from './components/wootsteam/WootSteam';
 import Footer from './components/footer/Footer';
 
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <Header />
       <WootBlock />
-      <EmailSignup />
+      <HonedByStone />
       <WootSteam />
       <Footer />
     </div>
