d9fabffd43dae1f3e02eae7795f9b2dd65160c64
George Miranda <george92miranda@gmail.com>
2023-05-08 23:40:00 -0700

    add iframe title

--- src/components/HonedByStone/HonedByStone.tsx
+++ src/components/HonedByStone/HonedByStone.tsx
@@ -4,7 +4,7 @@ import './HonedByStone.css';
 
 const HonedByStone = () =>
 <div className="HBS_container">
-    <iframe frameBorder={"0"} src="https://itch.io/embed/2032960?linkback=true&amp;bg_color=e1ddcb&amp;fg_color=222222&amp;link_color=EF8A4F&amp;border_color=b4a350" width="552" height="167"><a href="https://dogma-games.itch.io/honed-by-stone">Honed By Stone by Dogma Games</a></iframe>
+    <iframe title="honed-by-stone-widget" frameBorder={"0"} src="https://itch.io/embed/2032960?linkback=true&amp;bg_color=e1ddcb&amp;fg_color=222222&amp;link_color=EF8A4F&amp;border_color=b4a350" width="552" height="167"><a href="https://dogma-games.itch.io/honed-by-stone">Honed By Stone by Dogma Games</a></iframe>
     <p>
         Test out my very basic endless runner game for <span>FREE</span>. Dodge and slash your way past the flying rocks and try to score as high as you can.
     </p>
